e4e390aff160f610e02c95755ce7ea4ec269cd6d
Daniel Melo Alencar <danielmalencar2011@gmail.com>
2024-11-02 03:22:21 -0300

    Delete tsconfig-paths-bootstrap.js

--- tsconfig-paths-bootstrap.js
+++
@@ -1,7 +0,0 @@
-import tsConfigPaths from 'tsconfig-paths';
-import tsConfig from './tsconfig.json';
-
-tsConfigPaths.register({
-  baseUrl: tsConfig.compilerOptions.baseUrl,
-  paths: tsConfig.compilerOptions.paths,
-});
